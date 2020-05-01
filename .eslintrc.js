2052d5baa21c51eb755518126aa7cb3e6a29e985
leonardo rodrigues <leu_rr1@hotmail.com>
2020-04-30 22:42:27 -0300

    Estrutura inicial

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    "react/jsx-props-no-spreading": "off",
+    "prettier/prettier": "error",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".tsx"] }],
+    "import/prefer-default-export": "off",
+    "no-unused-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        "allowExpressions": true
+      }
+    ],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
+    'react/jsx-indent': 'off',
+    'react/jsx-indent-props': 'off'
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
+};
